67c56f611871253345635d39103431cd9d99aaad
Jaden Nation <jnation@revolt.tv>
2017-03-04 16:51:23 -0800

    Initial Commit

---
+++ SimpleParallax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleParallax
+//
+//  Created by Jaden Nation on 3/4/17.
+//  Copyright © 2017 Jaden Nation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
